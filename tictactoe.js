ea8248f5b58d8217d50ef7c9f023cfbcfd4ab3e1
Edvard A. M. Haaland <fishurban@gmail.com>
2017-05-04 18:00:19 +0200

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,117 @@
+// Grid of 3x3
+// Player VS. Computer
+// START GAME
+// Randomly selects who starts first
+var gameRunning = false;
+var turnOrder = 0;
+
+function StartGame(){
+	if (!gameRunning){
+		console.log("Game starting!");
+		ResetGame();
+		gameRunning=true;
+	}
+	else{
+		console.log("Game already running!");
+		return;
+	}
+}
+
+function ResetGame(){
+	for(var i = 0; i < 9; i++){
+		document.getElementsByClassName('all')[i].innerHTML = ""
+	}
+}
+
+function SelectBox(box){
+	if (turnOrder==0){
+		playerX(box);
+	}
+	if (turnOrder==1)
+	{
+		playerO(box);
+	}
+}
+
+function playerX(box){
+	if (!gameRunning){
+		console.log("Game isn't running!");
+		return;
+	}
+	else {
+		if (document.getElementById(box).innerHTML == ""){
+			document.getElementById(box).innerHTML="x";
+			console.log("Taking spot!");
+			CheckBoard(box, "x");
+			turnOrder=1;
+		}
+		else {
+			console.log("Spot already occupied!");
+		}
+	}
+}
+
+function playerO(box){
+	if (!gameRunning){
+		console.log("Game isn't running!");
+		return;
+	}
+	else {
+		if (document.getElementById(box).innerHTML == ""){
+			document.getElementById(box).innerHTML="o";
+			console.log("Taking spot!");
+			CheckBoard(box, "o");
+			turnOrder=0;
+		}
+		else {
+			console.log("Spot already occupied!");
+		}
+	}
+}
+
+function CheckBoard(box, player){
+	if (document.getElementById(box).className.includes("top")){
+		CheckRow('top', player);
+	}
+	if (document.getElementById(box).className.includes("mid")){
+		CheckRow('mid', player);
+	}
+	if (document.getElementById(box).className.includes("bot")){
+		CheckRow('bot', player);
+	}
+	if (document.getElementById(box).className.includes("right")){
+		CheckRow('right', player);
+	}
+	if (document.getElementById(box).className.includes("left")){
+		CheckRow('left', player);
+	}
+	if (document.getElementById(box).className.includes("center")){
+		CheckRow('center', player);
+	}
+	if (document.getElementById(box).className.includes("desc")){
+		CheckRow('desc', player);
+	}
+	if (document.getElementById(box).className.includes("asc")){
+		CheckRow('asc', player);
+	}
+	else {
+		console.log("placed at ID: "+box);
+	}
+}
+
+function CheckRow(row, player){
+	var victoryCount = 0;
+	for(var i = 0; i < 3; i++){
+		if (document.getElementsByClassName(row)[i].innerHTML==player){
+			console.log(player+" is present on "+row+" ("+(victoryCount+1)+")");
+			victoryCount++;
+			if(victoryCount==3){
+				gameRunning=false;
+				alert("Player "+player+" has won!");
+			}
+		}
+		else{
+			return;
+		}
+	}
+}
